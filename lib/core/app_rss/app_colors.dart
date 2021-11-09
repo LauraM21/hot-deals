c606868c666d2cfacdd813d3b69e8f06805ce0df
hari117 <hari.hari.cj@gmail.com>
2021-11-09 11:00:33 +0530

    setuped app theam

---
+++ lib/core/app_rss/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background_color = Color(0xff252A39);
+  static const Color white_color = Color(0xffFFFFFF);
+  static const Color secondary_text_color_01 = Color(0xff767D92);
+  static const Color text_field_background_color_02 = Color(0xff2D3344);
+
+  static const Color text_field_background_color = Color(0xff2D3344);
+
+  static const LinearGradient primaryColor = LinearGradient(
+    begin: Alignment.bottomCenter,
+    end: Alignment.topCenter,
+    colors: [
+      Color(0xff00adb5),
+      Color(0xff00f3ff),
+    ],
+  );
+}
