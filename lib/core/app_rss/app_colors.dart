27869bde4a505a6180d5dcce8512d141d0c1e6c1
hari117 <hari.hari.cj@gmail.com>
2021-11-10 11:40:23 +0530

    finished login page

--- lib/core/app_rss/app_colors.dart
+++ lib/core/app_rss/app_colors.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color background_color = Color(0xff252A39);
-  static const Color white_color = Color(0xffFFFFFF);
-  static const Color secondary_text_color_01 = Color(0xff767D92);
-  static const Color text_field_background_color_02 = Color(0xff2D3344);
 
-  static const Color text_field_background_color = Color(0xff2D3344);
 
-  static const LinearGradient primaryColor = LinearGradient(
-    begin: Alignment.bottomCenter,
-    end: Alignment.topCenter,
-    colors: [
-      Color(0xff00adb5),
-      Color(0xff00f3ff),
-    ],
-  );
+
+  static const Color primaryColor = Color(0xff252A39);
+  static const Color backgroundColor = Color(0xff252A39);
+  static const Color primaryTextColor = Colors.black;
+  static const Color secondary_text_color = Color(0xff767D92);
+
+
+
 }
